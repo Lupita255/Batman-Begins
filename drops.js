437a435f534150e3499e55408f8710bb8cf4c613
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-02-10 17:32:42 -0600

    Update drops.js

--- drops.js
+++ drops.js
@@ -1,22 +1,28 @@
 class Drop{
-    constructor(x, y, width, height) {
+    constructor(x,y,r){
         var options={
-         restitution: 0.1,
-         friction: 0.001
-      };
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      World.add(world,this.body);
+            isStatic:false,
+            friction:0.1,
+            density:1,
+            restitution:0.05
+        }
+        this.body=Bodies.circle(x,y,r,options);
+        this.radius=r;
+        World.add(world,this.body);
     }
-display(){
-    var pos = this.body.position;
-    fill("blue");
-    ellipse(pos.x, pos.y, this.width, this.height);
-  }
-   update(){
-     if(this.body.position.y<height){
-        Matter.Body.setPosition, (this.body,{x: random(0,400),y: random(0,400)});
-  }
- }
+
+    display(){
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode (RADIUS);
+        ellipse(0,0,this.radius,this.radius);
+        pop();
+    }
+
+update(){
+    if(this.body.position.y>height){
+        Matter.Body.setPosition(this.body,{x:random(0,400),y:random(0,400)})
+    }
+}
 }
