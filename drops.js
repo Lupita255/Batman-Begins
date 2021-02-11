1d54d5077012d3a178b7f7e92d88beffe907e4b2
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-02-10 18:00:11 -0600

    Update drops.js

--- drops.js
+++ drops.js
@@ -1,22 +1,27 @@
-class Umbrella{
-    constructor(x, y) {
+class Drops{
+    constructor(x, y){
         var options = {
-            isStatic: true
+            friction: 0.001,
+            restitution: 0.1
         }
-        
-        this.image = loadImage("walking_1.png")
-        this.body = Bodies.circle(x, y, 50, options);
-        this.radius = 50;
+        this.body = Bodies.circle(x, y, 5, options);
+        this.radius = 5;
+        this.x = x;
+        this.y = y;
 
         World.add(world, this.body);
     }
 
+    update(){
+        if(this.body.position.y > height){
+            Matter.Body.setPosition(this.body, {x: random(0, 400), y: random(0, 400)})
+        }
+    }
+
     display(){
         var pos = this.body.position;
-      
-        push();
-        imageMode(CENTER);
-        image(this.image, pos.x, pos.y + 70, 300, 300);
-        pop();
+        fill("blue");
+        ellipseMode(RADIUS);
+        ellipse(pos.x, pos.y, this.radius, this.radius);
     }
-};
+}
